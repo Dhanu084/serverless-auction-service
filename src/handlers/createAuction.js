877dbc227bdbac98cf29f85ce670922bd23850cb
dhanu <dhanuu08@gmail.com>
2022-01-30 13:15:28 +0530

    added middleware middy

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,10 +1,15 @@
 import { v4 as uuid } from "uuid";
-const AWS = require("aws-sdk");
+import AWS from "aws-sdk";
+import middy from "@middy/core";
+import httpJsonBodyParser from "@middy/http-json-body-parser";
+import httpEventNormalizer from "@middy/http-event-normalizer";
+import httpErrorHandler from "@middy/http-error-handler";
+import createError from "http-errors";
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 async function createAuction(event, context) {
-  const { title } = JSON.parse(event.body);
+  const { title } = event.body;
   const auction = {
     id: uuid(),
     title,
@@ -17,7 +22,12 @@ async function createAuction(event, context) {
     Item: auction,
   };
 
-  await dynamodb.put(params).promise();
+  try {
+    await dynamodb.put(params).promise();
+  } catch (error) {
+    console.log(error);
+    throw new createError.InternalServerError(error);
+  }
   const response = {
     statusCode: 201,
     body: JSON.stringify(auction),
@@ -25,4 +35,7 @@ async function createAuction(event, context) {
   return response;
 }
 
-export const handler = createAuction;
+export const handler = middy(createAuction)
+  .use(httpJsonBodyParser())
+  .use(httpEventNormalizer())
+  .use(httpErrorHandler());
